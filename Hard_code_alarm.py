d0784607063b64284f9ca24924a50120404f4cd1
Cristofer6010 <88551911+Cristofer6010@users.noreply.github.com>
2021-08-17 09:10:06 -0700

    Create Hard_code_alarm.py

---
+++ Hard_code_alarm.py
@@ -0,0 +1,90 @@
+import time
+time2 = "J"
+num = 1.0
+num2 = 2.0
+num3 = 3.0
+def datetime1():
+    import datetime
+    return datetime.datetime.now()
+
+def alarm1():
+    while True:
+        a = 100
+        if a == 100:
+            from pygame import mixer
+            mixer.init()
+            mixer.music.load("drink_water.py.mp3")
+            mixer.music.play()
+            time.sleep(10)
+
+        from pygame import mixer
+        print("Please enter the valid code to stop the Water alarm :")
+        b = input()
+        if b == "Water":
+            mixer.music.stop()
+        if b == "Water":
+            with open("harry_exrcice.txt", 'a') as f:
+                f.write((' This time i drank Water -->> ') + str(datetime1()) + ('\n'))
+
+        if num == 1.0:
+            prog2()
+
+def alarm2():
+    while True:
+        c = 10
+        if c == 10:
+            from pygame import mixer
+
+            mixer.init()
+            mixer.music.load("eye_exercise.py.mp3")
+            mixer.music.play()
+            time.sleep(10)
+
+        from pygame import mixer
+        print("Please enter the valid code to stop the Eye alarm :")
+        d = input()
+        if d == "Eye":
+            mixer.music.stop()
+        if d == "Eye":
+            with open("hammad_exrcice.txt", 'a') as f:
+                f.write((' This time i do eye exercise -->> ') + str(datetime1()) + ('\n'))
+
+        if num2 == 2.0:
+            prog3()
+
+def alarm3():
+    while True:
+        e = 1
+        if e == 1:
+            from pygame import mixer
+
+            mixer.init()
+            mixer.music.load("physical_exercice.py.mp3")
+            mixer.music.play()
+            time.sleep(10)
+
+        from pygame import mixer
+        print("Please enter the valid code to stop the Physical Exercise alarm :")
+        fgh = input()
+        if fgh == "Physical":
+            mixer.music.stop()
+        if fgh == "Physical":
+            with open("rohan_exrcice.txt", 'a') as f:
+                f.write((' This time i do some physical exercise -->> ') + str(datetime1()) + ('\n'))
+
+        if num3 == 3.0:
+            prog1()
+
+def prog1():
+    sleep = time.sleep(60)
+    alarm1()
+
+def prog2():
+    sleep = time.sleep(66)
+    alarm2()
+
+def prog3():
+    sleep = time.sleep(70)
+    alarm3()
+
+prog1()
